784977c50b6f6bd2e48ef61ec9e224f0ebf17fc4
Qingfeng0623 <ye304810501@qq.com>
2016-12-30 12:23:32 +0800

    First
    课程管理简易界面，逻辑不是很对，教师和学员无法登录

---
+++ Course Manage System/Course Manage System/Form4.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Course_Manage_System
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        private void load()
+        {
+            dataGridView4.Columns[4].DefaultCellStyle.Format = "yyyy-MM-dd";
+            // TODO:  这行代码将数据加载到表“courseDBDataSet11.ClassSpeaker”中。您可以根据需要移动或删除它。
+            this.classSpeakerTableAdapter.Fill(this.courseDBDataSet11.ClassSpeaker);
+            try
+            {
+                // TODO:  这行代码将数据加载到表“courseDBDataSet1.ClassState”中。您可以根据需要移动或删除它。
+                this.classStateTableAdapter.Fill(this.courseDBDataSet1.ClassState);
+                // TODO:  这行代码将数据加载到表“courseDBDataSet1.CourseState”中。您可以根据需要移动或删除它。
+                this.courseStateTableAdapter.Fill(this.courseDBDataSet1.CourseState);
+                // TODO:  这行代码将数据加载到表“courseDBDataSet1.ClassSpeaker”中。您可以根据需要移动或删除它。
+                this.classSpeakerTableAdapter.Fill(this.courseDBDataSet1.ClassSpeaker);
+                // TODO:  这行代码将数据加载到表“courseDBDataSet1.ChooseClass”中。您可以根据需要移动或删除它。
+                this.chooseClassTableAdapter.Fill(this.courseDBDataSet1.ChooseClass);
+            }
+            catch (Exception ee)
+            { }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form5 form5 = new Form5();
+            form5.Show();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            load();
+        }
+
+        
+    }
+}
---
+++ Course Manage System/Course Manage System/Form5.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Course_Manage_System
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.studentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.courseDBDataSet2 = new Course_Manage_System.CourseDBDataSet2();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.chooseBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.studentTableAdapter = new Course_Manage_System.CourseDBDataSet2TableAdapters.StudentTableAdapter();
+            this.chooseTableAdapter = new Course_Manage_System.CourseDBDataSet2TableAdapters.ChooseTableAdapter();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.courseDBDataSet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chooseBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkBlue;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(439, 477);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 39);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Ok";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(49, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(837, 176);
+            this.dataGridView1.TabIndex = 30;
+            // 
+            // studentBindingSource
+            // 
+            this.studentBindingSource.DataMember = "Student";
+            this.studentBindingSource.DataSource = this.courseDBDataSet2;
+            // 
+            // courseDBDataSet2
+            // 
+            this.courseDBDataSet2.DataSetName = "CourseDBDataSet2";
+            this.courseDBDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(247, 275);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 27;
+            this.dataGridView2.Size = new System.Drawing.Size(501, 172);
+            this.dataGridView2.TabIndex = 31;
+            // 
+            // chooseBindingSource
+            // 
+            this.chooseBindingSource.DataMember = "Choose";
+            this.chooseBindingSource.DataSource = this.courseDBDataSet2;
+            // 
+            // studentTableAdapter
+            // 
+            this.studentTableAdapter.ClearBeforeFill = true;
+            // 
+            // chooseTableAdapter
+            // 
+            this.chooseTableAdapter.ClearBeforeFill = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(908, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 52);
+            this.button2.TabIndex = 32;
+            this.button2.Text = "添加/更新";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(806, 289);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(65, 52);
+            this.button3.TabIndex = 33;
+            this.button3.Text = "添加/更新";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Snow;
+            this.button4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(908, 137);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 35);
+            this.button4.TabIndex = 34;
+            this.button4.Text = "删除";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Snow;
+            this.button5.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.ForeColor = System.Drawing.Color.Black;
+            this.button5.Location = new System.Drawing.Point(806, 364);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(65, 35);
+            this.button5.TabIndex = 35;
+            this.button5.Text = "删除";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 27);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "学员信息";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(177, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 27);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "选课信息";
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Course_Manage_System.Properties.Resources._5546d03a6e14e8b9c6dc650250abdc3b;
+            this.ClientSize = new System.Drawing.Size(1006, 556);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form5";
+            this.Text = "登记界面";
+            this.Load += new System.EventHandler(this.Form5_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.courseDBDataSet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chooseBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private CourseDBDataSet2 courseDBDataSet2;
+        private System.Windows.Forms.BindingSource studentBindingSource;
+        private CourseDBDataSet2TableAdapters.StudentTableAdapter studentTableAdapter;
+        private System.Windows.Forms.BindingSource chooseBindingSource;
+        private CourseDBDataSet2TableAdapters.ChooseTableAdapter chooseTableAdapter;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
